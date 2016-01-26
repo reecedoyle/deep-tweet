85ec62b4012889154b92ce6e3983a36d05f1b94a
Kapil Syamasundar <kapil_syamasundar@hotmail.co.uk>
2016-01-26 14:37:06 +0000

    Tried early stopping. it sucks for weird reasons and reverted back to normal grid search.

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Main.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Main.scala
@@ -1,5 +1,7 @@
 package uk.ac.ucl.cs.mr.statnlpbook.assignment3
 
+import scala.collection.mutable
+
 /**
  * @author rockt
  */
@@ -9,24 +11,51 @@ object Main extends App {
    *
    * Problems 2/3/4: perform a grid search over the parameters below
    */
+  def epochHook(iter: Int, accLoss: Double): Unit = {
+//    println("Epoch %4d\tLoss %8.4f\tTrain Acc %4.2f\tDev Acc %4.2f".format(
+//      iter, accLoss, 100 * Evaluator(model, trainSetName), 100*Evaluator(model, validationSetName)))
+  }
+
+
+
   val learningRate = 0.01
   val vectorRegularizationStrength = 0.01
   val matrixRegularizationStrength = 0.0
   val wordDim = 10
   val hiddenDim = 10
 
+
   val trainSetName = "train"
   val validationSetName = "dev"
-  
-  val model: Model = new SumOfWordVectorsModel(wordDim, vectorRegularizationStrength)
+  var model: Model = new SumOfWordVectorsModel(wordDim, vectorRegularizationStrength)
   //val model: Model = new RecurrentNeuralNetworkModel(wordDim, hiddenDim, vectorRegularizationStrength, matrixRegularizationStrength)
+  // StochasticGradientDescentLearner(model, trainSetName, 100, learningRate, epochHook)
 
-  def epochHook(iter: Int, accLoss: Double): Unit = {
-    println("Epoch %4d\tLoss %8.4f\tTrain Acc %4.2f\tDev Acc %4.2f".format(
-      iter, accLoss, 100 * Evaluator(model, trainSetName), 100*Evaluator(model, validationSetName)))
+  val accuracyMatrix:mutable.HashMap[(Int,Double,Double), Double] = new mutable.HashMap[(Int,Double,Double), Double]()
+
+  val range = List(0.005,0.01, 0.05)
+
+  for (i <- 7 to 10 by 1){
+    for (j <- range)
+    {
+      model = new SumOfWordVectorsModel(i, j)
+      for (k <- range)
+      {
+        StochasticGradientDescentLearner(model, trainSetName, 50, k, epochHook)
+        accuracyMatrix += (i,j,k) -> 100*Evaluator(model,validationSetName)
+        model.vectorParams.clear()
+        model.vectorParams += "param_w" -> VectorParam(i)
+      }
+    }
+    println(i)
   }
+  println(accuracyMatrix.maxBy(_._2))
 
-  StochasticGradientDescentLearner(model, trainSetName, 100, learningRate, epochHook)
+  //println(accuracyMatrix)
+
+
+
+
 
 
 
--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/StochasticGradientDescentLearner.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/StochasticGradientDescentLearner.scala
@@ -6,6 +6,8 @@ package uk.ac.ucl.cs.mr.statnlpbook.assignment3
 object StochasticGradientDescentLearner extends App {
   def apply(model: Model, corpus: String, maxEpochs: Int = 10, learningRate: Double, epochHook: (Int, Double) => Unit): Unit = {
     val iterations = SentimentAnalysisCorpus.numExamples(corpus)
+    var prevDevScore = 0.0
+    var currentDevScore = 0.0
     for (i <- 0 until maxEpochs) {
       var accLoss = 0.0
       for (j <- 0 until iterations) {
