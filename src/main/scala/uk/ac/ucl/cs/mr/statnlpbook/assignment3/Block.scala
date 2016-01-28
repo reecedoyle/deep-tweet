da2b64cd4d82d4cd11e12aa3c69194f93256dd53
Tubbimora <oliverttun@hotmail.com>
2016-01-28 20:55:25 +0000

    Merge branch 'master' of C:\Users\GP62\NLP\deep-tweet with conflicts.

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Block.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Block.scala
@@ -341,7 +341,7 @@ case class Tanh(arg: Block[Vector]) extends Block[Vector] {
  * @param prob dropout probability
  * @param arg a block evaluating to a vector whose components we want to drop
  */
-case class Dropout(prob: Double, arg: Block[Vector]) extends Block[Vector] {
+case class DropoutSum(prob: Double, arg: Block[Vector]) extends Block[Vector] {
   var randnum = Random.nextDouble()
   val drop = {
     if (randnum > prob) {
@@ -382,6 +382,30 @@ case class Dropout(prob: Double, arg: Block[Vector]) extends Block[Vector] {
 /**
   * ... be free, be creative :)
   */
+
+/**
+  * A block representing the product of vectors
+  *
+  * @param args a sequence of blocks that evaluate to vectors
+  */
+case class Product(args: Seq[Block[Vector]]) extends Block[Vector] {
+  def forward(): Vector = {
+    output = args.tail.map(v => v.forward()).fold(args.head.forward()){(u,v) => u :* v}
+    output
+  }
+
+  //Pass product of every vector except the current arg
+  def backward(gradient: Vector): Unit = {
+    args.foreach(v => {
+      val forward = v.forward()
+      val temp = gradient :* args.tail.map(v => v.forward()).fold(args.head.forward()){(u,v) => u :* v} :/ forward
+      v.backward(temp)
+    })
+  }
+  def update(learningRate: Double): Unit = args.foreach(v => v.update(learningRate))
+}
+
+
 
 /**
  * A block representing the vertical concatenation of two vectors
--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Main.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment3/Main.scala
@@ -1,6 +1,6 @@
 package uk.ac.ucl.cs.mr.statnlpbook.assignment3
 
-import java.io.{FileOutputStream, File, PrintWriter}
+import scala.collection.mutable
 
 /**
  * @author rockt
@@ -11,50 +11,73 @@ object Main extends App {
    *
    * Problems 2/3/4: perform a grid search over the parameters below
    */
-  val learningRate = 0.05
-  val vectorRegularizationStrength = 0.05
+
+  val learningRate = 0.01
+  val vectorRegularizationStrength = 0.01
   val matrixRegularizationStrength = 0.0
-  val wordDim = 9
+  val wordDim = 10
   val hiddenDim = 10
+  val probability = 0.6
+
 
   val trainSetName = "train"
   val validationSetName = "dev"
-  
-  val model: Model = new SumOfWordVectorsModel(wordDim, vectorRegularizationStrength)
+  //var model: Model = new SumOfWordVectorsModel(wordDim, vectorRegularizationStrength)
+  //var model: Model = new SumOfWordVectorsModelWithDropout(wordDim, vectorRegularizationStrength, probability)
+  var model: Model = new ProductOfWordVectorsModel(wordDim, vectorRegularizationStrength)
   //val model: Model = new RecurrentNeuralNetworkModel(wordDim, hiddenDim, vectorRegularizationStrength, matrixRegularizationStrength)
 
-  // Put in word2vec representations for vectors into hashmap
-
-
   def epochHook(iter: Int, accLoss: Double): Unit = {
+    /*val weights = model.vectorParams("param_w")
+    val adjustedWeights = weights.copy()
+    adjustedWeights.param :* probability
+    val testModel = new SumOfWordVectorsModel(wordDim, vectorRegularizationStrength)
+    testModel.vectorParams ++= model.vectorParams
+    testModel.vectorParams += "param_w" -> adjustedWeights
+    model.vectorParams += "param_w" -> weights*/
     println("Epoch %4d\tLoss %8.4f\tTrain Acc %4.2f\tDev Acc %4.2f".format(
       iter, accLoss, 100 * Evaluator(model, trainSetName), 100*Evaluator(model, validationSetName)))
   }
 
-//  StochasticGradientDescentLearner(model, trainSetName, 100, learningRate, epochHook)
-  StochasticGradientDescentLearner(model, trainSetName, 100, learningRate, epochHook)
 
-  // Output words, vector representations of words and weights in separate text files
-//  model.vectorParams.remove("param_w") // This is not a word
+  StochasticGradientDescentLearner(model, trainSetName, 10, learningRate, epochHook)
 
-//  // Vector representations
-//  val vectorWriter = new PrintWriter(new FileOutputStream("vectorOutput.txt", false))
-//  val vectorStrings = model.vectorParams.map(e => e._2).map(f => f.forward().toArray.map(g => g.toString).mkString(", "))
-//  vectorStrings.foreach(e => vectorWriter.append(e + "\n"))
-//  vectorWriter.close()
-//
-//  // Scores
-//  val scoreWriter = new PrintWriter(new FileOutputStream("scoreOutput.txt", false))
-//  val scoreStrings = model.vectorParams.map(e => e._2).map(f => model.scoreSentence(f).forward().toString)
-//  scoreStrings.foreach(e => scoreWriter.append(e + "\n"))
-//  scoreWriter.close()
-//
-//  val tokenWriter = new PrintWriter(new FileOutputStream("tokenOutput.txt", false))
-//  val tokenStrings = model.vectorParams.map(e => e._1)
-//  tokenStrings.foreach(e => tokenWriter.append(e + "\n"))
-//  tokenWriter.close()
-//
-//  model.vectorParams.get("")
+  /*var model: Model = new SumOfWordVectorsModelWithDropout(wordDim, vectorRegularizationStrength, probability)
+
+  val accuracyMatrix:mutable.HashMap[(Int,Double,Double), Double] = new mutable.HashMap[(Int,Double,Double), Double]()
+
+  val range = List(0.005,0.01, 0.05)
+  //val range = List(0.005)
+  for (i <- 7 to 10 by 1){
+    for (j <- range)
+    {
+      model = new SumOfWordVectorsModelWithDropout(i, j, probability)
+      for (k <- range)
+      {
+        StochasticGradientDescentLearner(model, trainSetName, 50, k, epochHook)
+
+        val weights = model.vectorParams("param_w")
+        weights.param :* probability
+        val testModel = new SumOfWordVectorsModel(i, j)
+        testModel.vectorParams ++= model.vectorParams
+        testModel.vectorParams += "param_w" -> weights
+
+        accuracyMatrix += (i,j,k) -> 100*Evaluator(model,validationSetName)
+        model.vectorParams.clear()
+        model.vectorParams += "param_w" -> VectorParam(i)
+      }
+    }
+    println(i)
+  }
+  println(accuracyMatrix.maxBy(_._2))
+
+  println(accuracyMatrix)*/
+
+
+
+
+
+
 
   /**
    * Comment this in if you want to look at trained parameters
